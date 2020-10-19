91326dbaacc3756446a9a9e7b7da850f8ce2570b
Kimjuu <point8622@gmail.com>
2020-10-19 17:41:56 +0900

    static 경로설정, 이미지경로 패스설정

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,131 +1,149 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import $ from 'jquery';
-import Swal from 'sweetalert2'
+import $ from "jquery";
+import Swal from "sweetalert2";
 
 class Main extends Component {
-
   submitClick = async (e) => {
+    this.qna_name = $("#qna_name").val();
+    this.qna_hp = $("#qna_hp").val();
+    this.qna_content = $("#qna_content").val();
 
-        this.qna_name = $('#qna_name').val();
-        this.qna_hp = $('#qna_hp').val();
-        this.qna_content = $('#qna_content').val();
-
-        this.fnValidate = (e) => {
-            if(this.qna_name === '') {
-              alert('이름을 다시 확인해주세요.')
-              return false;
-            }else if(this.qna_hp === ''){
-              alert('핸드폰 번호를 다시 확인해주세요.')
-              return false;
-            }else if(this.qna_content === ''){
-              alert('문의내용을 다시 확인해주세요.')
-              return false;
-            }
-            return true;
-        }
-
-        if(this.fnValidate()){
-            var jsonstr = $("form[name='frm']").serialize();
-            jsonstr = decodeURIComponent(jsonstr);
-            var Json_form = JSON.stringify(jsonstr).replace(/\"/gi,'')
-            Json_form = "{\"" +Json_form.replace(/\&/g,'\",\"').replace(/=/gi,'\":"')+"\"}";
-        
-            try {
-                const response = await fetch('/qnaregister', {
-                    method: 'POST',
-                    headers: {
-                    'Content-Type': 'application/json',
-                    },
-                    body: Json_form,
-                });
-                const body = await response.text();
-                if(body == "succ"){
-                    this.sweetalertSucc('문의가 완료되었습니다.', false)
-
-                    setTimeout(function() {
-                        this.props.history.push('/');
-                        }.bind(this),3000
-                    );
-                }else{
-                    alert('작업중 오류가 발생하였습니다.')
-                }  
-            } catch (error) {
-                alert('작업중 오류가 발생하였습니다.')
-            }
-        }
+    this.fnValidate = (e) => {
+      if (this.qna_name === "") {
+        alert("이름을 다시 확인해주세요.");
+        return false;
+      } else if (this.qna_hp === "") {
+        alert("핸드폰 번호를 다시 확인해주세요.");
+        return false;
+      } else if (this.qna_content === "") {
+        alert("문의내용을 다시 확인해주세요.");
+        return false;
+      }
+      return true;
     };
 
-    
-    sweetalertSucc = (title, showConfirmButton) => {
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: title,
-            showConfirmButton: showConfirmButton,
-            timer: 1000
-        })
+    if (this.fnValidate()) {
+      var jsonstr = $("form[name='frm']").serialize();
+      jsonstr = decodeURIComponent(jsonstr);
+      var Json_form = JSON.stringify(jsonstr).replace(/\"/gi, "");
+      Json_form =
+        '{"' + Json_form.replace(/\&/g, '","').replace(/=/gi, '":"') + '"}';
+
+      try {
+        const response = await fetch("/qnaregister", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: Json_form,
+        });
+        const body = await response.text();
+        if (body == "succ") {
+          this.sweetalertSucc("문의가 완료되었습니다.", false);
+
+          setTimeout(
+            function () {
+              this.props.history.push("/");
+            }.bind(this),
+            3000
+          );
+        } else {
+          alert("작업중 오류가 발생하였습니다.");
+        }
+      } catch (error) {
+        alert("작업중 오류가 발생하였습니다.");
+      }
     }
+  };
+
+  sweetalertSucc = (title, showConfirmButton) => {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: title,
+      showConfirmButton: showConfirmButton,
+      timer: 1000,
+    });
+  };
 
   render() {
     return (
       <section id="main">
         <section className="thumbnails">
           <div>
-            <Link to={require("../images/suseok/suseok4.jpg")}>
-              <img src={require("../images/suseok/suseok4.jpg")} alt="" />
+            <Link to="suseok/suseok1.jpg">
+              <img src="/suseok/suseok1.jpg" alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
-            <Link to={require("../images/suseok/suseok1.jpg")}>
-              <img src={require("../images/suseok/suseok1.jpg")} alt="" />
+          </div>
+
+                    
+          <div>
+            <Link to={require("/suseok/suseok4.jpg")}>
+              <img src={require("/suseok/suseok4.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
-            <Link to={require("../images/suseok/suseok2.jpg")}>
-              <img src={require("../images/suseok/suseok2.jpg")} alt="" />
+            <Link to={require("/suseok/suseok1.jpg")}>
+              <img src={require("/suseok/suseok1.jpg")} alt="" />
+              <h3>Lorem ipsum dolor sit amet</h3>
+            </Link>
+            <Link to={require("/suseok/suseok2.jpg")}>
+              <img src={require("/suseok/suseok2.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
           </div>
           <div>
-            <Link to={require("../images/suseok/suseok3.jpg")}>
-              <img src={require("../images/suseok/suseok3.jpg")} alt="" />
+            <Link to={require("/suseok/suseok3.jpg")}>
+              <img src={require("/suseok/suseok3.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
-            <Link to={require("../images/suseok/suseok5.jpg")}>
-              <img src={require("../images/suseok/suseok5.jpg")} alt="" />
+            <Link to={require("/suseok/suseok5.jpg")}>
+              <img src={require("/suseok/suseok5.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
-            <Link to={require("../images/suseok/suseok3.jpg")}>
-              <img src={require("../images/suseok/suseok3.jpg")} alt="" />
+            <Link to={require("/suseok/suseok3.jpg")}>
+              <img src={require("/suseok/suseok3.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
-            <Link to={require("../images/suseok/suseok3.jpg")}>
-              <img src={require("../images/suseok/suseok3.jpg")} alt="" />
+            <Link to={require("/suseok/suseok3.jpg")}>
+              <img src={require("/suseok/suseok3.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
           </div>
           <div>
-            <Link to={require("../images/suseok/suseok4.jpg")}>
-              <img src={require("../images/suseok/suseok4.jpg")} alt="" />
+            <Link to={require("/suseok/suseok4.jpg")}>
+              <img src={require("/suseok/suseok4.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
-            <Link to={require("../images/suseok/suseok1.jpg")}>
-              <img src={require("../images/suseok/suseok1.jpg")} alt="" />
+            <Link to={require("/suseok/suseok1.jpg")}>
+              <img src={require("/suseok/suseok1.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
-            <Link to={require("../images/suseok/suseok2.jpg")}>
-              <img src={require("../images/suseok/suseok2.jpg")} alt="" />
+            <Link to={require("/suseok/suseok2.jpg")}>
+              <img src={require("/suseok/suseok2.jpg")} alt="" />
               <h3>Lorem ipsum dolor sit amet</h3>
             </Link>
           </div>
         </section>
         <section>
-          <form name="frm" id="frm" action="" onsubmit="" method="post" >
+          <form name="frm" id="frm" action="" onsubmit="" method="post">
             <div className="row uniform">
               <div className="6u 12u$(xsmall)">
-                <input name="qna_name" id="qna_name" placeholder="Name" type="text" />
+                <input
+                  name="qna_name"
+                  id="qna_name"
+                  placeholder="Name"
+                  type="text"
+                />
               </div>
               <div className="6u$ 12u$(xsmall)">
-                <input name="qna_hp" id="qna_hp" placeholder="Email" type="text" />
+                <input
+                  name="qna_hp"
+                  id="qna_hp"
+                  placeholder="Email"
+                  type="text"
+                />
               </div>
               <div className="12u$">
                 <textarea
@@ -137,7 +155,13 @@ class Main extends Component {
             </div>
             <ul className="actions">
               <li>
-                <a href="javascript:" className="alt" onClick={(e) => this.submitClick('save', e)}>문의하기</a>
+                <a
+                  href="javascript:"
+                  className="alt"
+                  onClick={(e) => this.submitClick("save", e)}
+                >
+                  문의하기
+                </a>
               </li>
             </ul>
           </form>
--- server.js
+++ server.js
@@ -8,5 +8,7 @@ var app = express();
 app.use('/', indexRouter);
 app.use('/qnaregister', qnaRouter);
 
+app.use(express.static("./uploads"));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
